5cecf6cd7a78221e43daea6a226f0acab2dc196c
Elyran Kogan <elyran@liveperson.com>
2016-05-30 17:35:10 +0300

    add integration test

--- src/test/java/com/liveperson/tutorial/spring/boot/rest/it/HelloControllerItTest.java
+++ src/test/java/com/liveperson/tutorial/spring/boot/rest/it/HelloControllerItTest.java
@@ -1,5 +1,6 @@
-package com.liveperson.tutorial.spring.boot.rest;
+package com.liveperson.tutorial.spring.boot.rest.it;
 
+import com.liveperson.tutorial.spring.boot.rest.Application;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
