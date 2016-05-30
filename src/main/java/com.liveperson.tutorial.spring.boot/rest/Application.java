853beaf6d791bfbdd5a5797051b773894c47e7c5
Elyran Kogan <elyran@liveperson.com>
2016-05-30 14:10:42 +0300

    initial project

---
+++ src/main/java/com.liveperson.tutorial.spring.boot/rest/Application.java
@@ -0,0 +1,28 @@
+package com.liveperson.tutorial.spring.boot.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+/**
+ * @author elyran
+ * @since 5/30/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
+
+}
---
+++ src/main/java/com.liveperson.tutorial.spring.boot/rest/HelloController.java
@@ -0,0 +1,19 @@
+package com.liveperson.tutorial.spring.boot.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author elyran
+ * @since 5/30/16.
+ */
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
