d1df0421b58ec52f846df1884efa53d05e2f93b6
Elyran Kogan <elyran@liveperson.com>
2016-05-30 14:15:39 +0300

    initial project

--- src/main/java/com.liveperson.tutorial.spring.boot/rest/Application.java
+++ src/main/java/com.liveperson.tutorial.spring.boot/rest/Application.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import java.util.Arrays;
-
 /**
  * @author elyran
  * @since 5/30/16.
@@ -16,13 +14,6 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
     }
 
 }
--- src/main/java/com.liveperson.tutorial.spring.boot/rest/HelloController.java
+++ src/main/java/com.liveperson.tutorial.spring.boot/rest/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot! ";
     }
 
 }
