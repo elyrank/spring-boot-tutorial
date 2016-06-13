a0954d5df6a35c829d2197e9612e37774da6f1ce
Elyran Kogan <elyran@liveperson.com>
2016-06-13 11:51:38 +0300

    full app

--- src/main/java/com/liveperson/tutorial/spring/boot/jdbc/PersonRepository.java
+++ src/main/java/com/liveperson/tutorial/spring/boot/jdbc/PersonRepository.java
@@ -2,6 +2,7 @@ package com.liveperson.tutorial.spring.boot.jdbc;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Collection;
 
@@ -9,7 +10,8 @@ import java.util.Collection;
  * @author elyran
  * @since 6/5/16.
  */
+@RepositoryRestResource
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
-    Collection<Person> findPersonByName(String name);
+    Collection<Person> findPersonByName(@Param("name") String name);
 }
