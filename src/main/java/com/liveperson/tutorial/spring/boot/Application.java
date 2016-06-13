ed17352554158f236f1b1494ac904b7aedb402ae
Elyran Kogan <elyran@liveperson.com>
2016-06-13 14:11:39 +0300

    step1

--- src/main/java/com/liveperson/tutorial/spring/boot/Application.java
+++ src/main/java/com/liveperson/tutorial/spring/boot/Application.java
@@ -4,14 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 /**
  * @author elyran
  * @since 5/30/16.
  */
 @SpringBootApplication
-@EnableWebSocket
 public class Application {
 
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
