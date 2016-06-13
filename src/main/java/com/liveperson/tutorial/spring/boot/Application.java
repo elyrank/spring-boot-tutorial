1bb13b1ae8b5039bb9385ded42f4658a82d23712
Elyran Kogan <elyran@liveperson.com>
2016-06-13 11:45:17 +0300

    full app

--- src/main/java/com/liveperson/tutorial/spring/boot/Application.java
+++ src/main/java/com/liveperson/tutorial/spring/boot/Application.java
@@ -1,15 +1,13 @@
 package com.liveperson.tutorial.spring.boot;
 
-import com.liveperson.tutorial.spring.boot.jdbc.PersonRepository;
 import com.liveperson.tutorial.spring.boot.jdbc.Person;
+import com.liveperson.tutorial.spring.boot.jdbc.PersonRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 import java.util.Arrays;
 
