b30aaf8f83f9512ec864bb083f2211df0f7e83cd
Elyran Kogan <elyran@liveperson.com>
2016-06-13 12:19:34 +0300

    step4 - add custom counter

--- src/main/java/com/liveperson/tutorial/spring/boot/ws/WsHandler.java
+++ src/main/java/com/liveperson/tutorial/spring/boot/ws/WsHandler.java
@@ -2,7 +2,9 @@ package com.liveperson.tutorial.spring.boot.ws;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -18,6 +20,8 @@ public class WsHandler extends TextWebSocketHandler {
     @Value("${ws.message}")
     private String wsMessage;
 
+    @Autowired
+    CounterService counterService;
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
@@ -30,6 +34,8 @@ public class WsHandler extends TextWebSocketHandler {
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         logger.info("received message: {}", message);
+
+        counterService.increment("ws.total.messages");
         session.sendMessage(new TextMessage(wsMessage + " -- " + message.getPayload()));
     }
 
