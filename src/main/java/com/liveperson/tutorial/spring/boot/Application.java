c9e204fcabb204a85ce01506730efda1f90f85e1
Elyran Kogan <elyran@liveperson.com>
2016-06-13 08:09:33 +0300

    step1

--- src/main/java/com/liveperson/tutorial/spring/boot/Application.java
+++ src/main/java/com/liveperson/tutorial/spring/boot/Application.java
@@ -1,17 +1,10 @@
 package com.liveperson.tutorial.spring.boot;
 
-import com.liveperson.tutorial.spring.boot.jdbc.PersonRepository;
-import com.liveperson.tutorial.spring.boot.jdbc.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-
-import java.util.Arrays;
 
 /**
  * @author elyran
@@ -26,28 +19,6 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
-    }
-
-    @Bean
-    CommandLineRunner runner(PersonRepository personRepository) {
-        return args -> {
-            Arrays.asList("Moshe,Danny,Shmulik".split(","))
-            .forEach(n -> personRepository.save(new Person(n)));
-
-            //test it:
-            personRepository.findAll().forEach(p -> logger.info("person: {}", p));
-
-            personRepository.findPersonByName("Danny")
-            .forEach(p -> logger.info("found person by name: {}", p));
-        };
-    }
-
-
-
-
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
     }
 
 }
